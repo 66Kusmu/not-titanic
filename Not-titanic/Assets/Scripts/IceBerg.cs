5cb352a2ee0107cdd8739ffbfdebac04141c0f02
Konsta Karttunen <ekoka001@edu.xamk.fi>
2024-04-05 22:15:52 +0300

    Iceberg works

--- Not-titanic/Assets/Scripts/IceBerg.cs
+++ Not-titanic/Assets/Scripts/IceBerg.cs
@@ -16,16 +16,9 @@ public class IceBerg : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        //first we make sure that the object that hit the banana is the player.
-        if (collision.gameObject.tag == "Player")
-        {
-            // This will search all player scripts for a function called "Hit Banana"
-            Debug.Log("Banana was hit by player");
-        }
+        //Iceberg needs collider and rigidbody
+        Debug.Log(other.gameObject.tag + " has triggered");
     }
-
-    
-
 }
