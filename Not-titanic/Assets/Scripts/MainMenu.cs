148d179fec41aee5e4313f754f6cadc260e43355
fvela012 <fvela012@edu.xamk.fi>
2024-04-06 17:17:30 +0300

    Start button

---
+++ Not-titanic/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=DX7HyN7oJjE Unity main menu tutorial
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+
+    }
+
+}
