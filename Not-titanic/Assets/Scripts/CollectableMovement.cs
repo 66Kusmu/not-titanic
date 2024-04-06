c35cae4c2ed8dfe296c20cfee4868f672b0204cf
Konsta Karttunen <ekoka001@edu.xamk.fi>
2024-04-06 13:45:46 +0300

    NavMesh complete

--- Not-titanic/Assets/Scripts/CollectableMovement.cs
+++ Not-titanic/Assets/Scripts/CollectableMovement.cs
@@ -5,29 +5,25 @@ using UnityEngine.AI;
 
 public class CollectableMovement : MonoBehaviour
 {
-    private List<Transform> points;
+    private GameObject[] points;
     private NavMeshAgent agent;
     
     // Start is called before the first frame update
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
-        GameObject go = GameObject.FindGameObjectWithTag("WayPoints");
-        foreach (Transform t in go.transform)
-        {
-            points.Add(t);
-        }
+        points = GameObject.FindGameObjectsWithTag("WayPoints");
 
-        agent.SetDestination(points[Random.Range(0, points.Count)].position);
+        agent.SetDestination(points[Random.Range(0, points.Length)].transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(agent.destination + " " + agent.remainingDistance);
+        //Debug.Log(agent.destination + " " + agent.remainingDistance);
         if (agent.remainingDistance <= agent.stoppingDistance)
         {
-            agent.SetDestination(points[Random.Range(0, points.Count)].position);
+            agent.SetDestination(points[Random.Range(0, points.Length)].transform.position);
         }
     }
 }
